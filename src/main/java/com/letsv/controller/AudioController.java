9827bf2637014165962c7401cf56408624ab0e15
jimgao123456 <408895099@qq.com>
2018-05-22 12:53:36 +0800

    添加语音接口

---
+++ src/main/java/com/letsv/controller/AudioController.java
@@ -0,0 +1,27 @@
+package com.letsv.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+public class AudioController {
+	@PostMapping("/audio")
+	public @ResponseBody
+	Map<String,Object> login(int number) {
+
+		Map<String, Object> map = new HashMap<>();
+		String word="apple";
+		switch (number){
+			case 1:
+				word="apple";
+				break;
+		}
+		map.put("url","http://media.shanbay.com/audio/us/"+word+".mp3");
+		return map;
+	}
+}
